4d1e25cd9ed6506f4e915bf5b66a9f204922e970
hyoju11 <gywn32@naver.com>
2017-11-20 01:18:58 +0900

    first assignment

---
+++ 1주차 과제/src/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO-generated method stub
+int lineNumber;
+lineNumber=1;
+System.out.println(lineNumber+".세상에");
+lineNumber=2;
+System.out.println(lineNumber+".강변에 달빛이 곱다고");
+lineNumber=3;
+System.out.println(lineNumber+".전화를 다 주시다니요");
+lineNumber=4;
+System.out.println(lineNumber+".흐르는 물 어디쯤 눈부시게 부서지는 소리");
+lineNumber=5;
+System.out.println(lineNumber+".문득 들려옵니다");
+lineNumber=6;
+System.out.println(lineNumber+".");
+lineNumber=7;
+System.out.println(lineNumber+".김용택/달이 떴다고 전화를 주시다니요");
+
+
+
+	}
+
+}
